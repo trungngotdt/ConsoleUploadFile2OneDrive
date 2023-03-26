1e867c9552e9796af6289805e0a33efd6b87baf1
trung ngo <trungngotdt@outlook.com.vn>
2023-03-26 15:38:47 +0700

    commit

--- ConsoleUploadFile2OneDrive/Program.cs
+++ ConsoleUploadFile2OneDrive/Program.cs
@@ -1,2 +1,72 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using FileLibrary;
+
+namespace ConsoleUploadFile2OneDrive // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string parameterFolderPath = "--path";
+            const string parameterClientID = "--clientid";
+            const string parameterClientSecret = "--clientsecret";
+            const string parameterUPN = "--upn";
+            const string parameterTenantId = "--tenantid";
+            const string parameterOutput = "--output";
+
+            Console.WriteLine("Working");
+           
+            string path = String.Empty;
+            string clientID = string.Empty;
+            string upn = String.Empty;
+            string clientSecret = string.Empty;
+            string output = String.Empty;
+            string tenantid = string.Empty;
+
+
+            var length = args.Length;
+
+            for (int i = 0; i < length; i++)
+            {
+                switch (args[i].Trim().ToLower())
+                {
+                    case parameterClientID:
+                        clientID = args[i + 1];
+                        break;
+                    case parameterClientSecret:
+                        clientSecret = args[i + 1];
+                        break;
+                    case parameterFolderPath:
+                        path = args[i + 1];
+                        break;
+                    case parameterOutput:
+                        output = args[i + 1];
+                        break;
+                    case parameterTenantId:
+                        tenantid = args[i + 1];
+                        break;
+                    case parameterUPN:
+                        upn = args[i + 1];
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+            if ( string.IsNullOrEmpty(clientID) || string.IsNullOrEmpty(clientSecret) || string.IsNullOrEmpty(path)
+                || string.IsNullOrEmpty(output) || string.IsNullOrEmpty(tenantid) || string.IsNullOrEmpty(upn))
+            {
+                Console.WriteLine("Empty parameters!");
+                return;
+            }
+            (new FileUtils(clientID, clientSecret, tenantid, path, upn, output)).Upload();
+        }
+    }
+}
---
+++ FileLibrary/FileUtils.cs
@@ -0,0 +1,160 @@
+﻿using Azure.Identity;
+using Microsoft.Graph;
+using Microsoft.Graph.Drives.Item.Items.Item.CreateUploadSession;
+using Microsoft.Graph.Models;
+using System.IO;
+
+namespace FileLibrary
+{
+    public class FileUtils
+    {
+        private GraphServiceClient _graphClient;
+        private string _clientID;
+        private string _clientSecret;
+        private string _upn;
+        private string _folderPath;
+        private string _tenantId;
+        private string _folderOutput;
+        public FileUtils(string clientID, string clientSecret, string tenantId, string folderPath, string Upn, string folderOutput)
+        {
+            _clientID = clientID;
+            _clientSecret = clientSecret;
+            TenantId = tenantId;
+            _folderPath = folderPath;
+            _upn = Upn;
+            _folderOutput = folderOutput;
+            
+        }
+
+        public GraphServiceClient graphClient { get => _graphClient; set => _graphClient = value; }
+        public string ClientId { get => _clientID; set => _clientID = value; }
+        public string ClientSecret { get => _clientSecret; set => _clientSecret = value; }
+        public string UPN { get => _upn; set => _upn = value; }
+        public string FolderPath { get => _folderPath; set => _folderPath = value; }
+        public string TenantId { get => _tenantId; set => _tenantId = value; }
+        public string FolderOutput { get => _folderOutput; set => _folderOutput = value; }
+
+        private void createClient()
+        {
+            var scopes = new[] { "https://graph.microsoft.com/.default" };
+
+            var options = new TokenCredentialOptions
+            {
+                AuthorityHost = AzureAuthorityHosts.AzurePublicCloud
+            };
+
+            // https://learn.microsoft.com/dotnet/api/azure.identity.clientsecretcredential
+            var clientSecretCredential = new ClientSecretCredential(
+                TenantId, ClientId, ClientSecret, options);
+
+            graphClient = new GraphServiceClient(clientSecretCredential, scopes);
+
+        }
+
+        public async Task Upload()
+        {
+            try
+            {
+                FileAttributes attr = File.GetAttributes(FolderPath);
+                createClient();
+                if (attr.HasFlag(FileAttributes.Directory))
+                {
+                    await UploadFolderAsync(FolderPath);
+                }
+                else
+                {
+                   await UploadFileAsync(FolderPath);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error uploading: {ex.ToString()}");
+            }
+        }
+        private List<String> dirSearch(string sDir)
+        {
+            List<String> files = new List<String>();
+            try
+            {
+                foreach (string f in Directory.GetFiles(sDir))
+                {
+                    files.Add(f);
+                }
+                foreach (string d in Directory.GetDirectories(sDir))
+                {
+                    files.AddRange(dirSearch(d));
+                }
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine($"Error uploading: {ex.ToString()}");
+            }
+
+            return files;
+        }
+        public async Task UploadFolderAsync(string folder)
+        {
+            var tasks = new List<Task>();
+            
+            var files = dirSearch(folder);
+            foreach (var f in files)
+            {
+               tasks.Add( UploadFileAsync(f));
+            }
+            await Task.WhenAll(tasks);
+        }
+
+        public async Task UploadFileAsync(string pathFile)
+        {
+            
+            using var fileStream = System.IO.File.OpenRead(pathFile);
+
+            // Use properties to specify the conflict behavior
+            // in this case, replace
+            var uploadSessionRequestBody = new CreateUploadSessionPostRequestBody
+            {
+                Item = new DriveItemUploadableProperties
+                {
+                    AdditionalData = new Dictionary<string, object>
+                    {
+                        { "@microsoft.graph.conflictBehavior", "replace" }
+                    }
+                }
+            };
+            var driveItem = await graphClient.Users[UPN].Drive.GetAsync();
+            // Create the upload session
+            // itemPath does not need to be a path to an existing item
+            var uploadSession = await graphClient.Drives[driveItem.Id].Root
+                .ItemWithPath(Path.GetFileName(Path.Combine(FolderPath, Path.GetFileName( fileStream.Name))))
+                .CreateUploadSession
+                .PostAsync(uploadSessionRequestBody);
+
+            // Max slice size must be a multiple of 320 KiB
+            int maxSliceSize = 320 * 1024;
+            var fileUploadTask = new LargeFileUploadTask<DriveItem>(uploadSession, fileStream, maxSliceSize, graphClient.RequestAdapter);
+
+            var totalLength = fileStream.Length;
+            // Create a callback that is invoked after each slice is uploaded
+            IProgress<long> progress = new Progress<long>(prog =>
+            {
+                Console.WriteLine($"{Path.GetFileName(fileStream.Name)} Uploaded {prog} bytes of {totalLength} bytes");
+            });
+
+            try
+            {
+                // Upload the file
+                var uploadResult = await fileUploadTask.UploadAsync(progress);
+
+                Console.WriteLine(uploadResult.UploadSucceeded ?
+                    $"Upload complete, item ID: {uploadResult.ItemResponse.Id}" :
+                    "Upload failed");
+            }
+            catch (ServiceException ex)
+            {
+                Console.WriteLine($"Error uploading: {ex.ToString()}");
+            }
+        }
+
+    }
+}
